cd9bd2bababb8d85b43e0c871cab3831817d91cd
tolo137 <matthewj.egan@hotmail.com>
2014-01-27 23:37:28 +1100

    Fixed tree fractal base branch lengths

--- treeFractal.py
+++ treeFractal.py
@@ -1,13 +1,16 @@
 import turtle
 
-angle = 90
+angle = 30
 divF = 0.7
-startSize = 100
-startDepth = 14
+startSize = 175
+startDepth = 13
+height = 275
 
 def main():
+    prompt = raw_input()
     t = turtle.Turtle()
-    turtle.setup(width=1280, height=600)
+    turtle.setup(width=1280, height=1280)
+    t.hideturtle()
     turtle.colormode(255)
     ##turtle.bgcolor(0, 0, 0)
     ##t.fillcolor(0, 255, 0)
@@ -15,8 +18,10 @@ def main():
     t.speed(0)
     t.penup()
     t.left(90)
-    t.backward(startSize)
+    t.backward(height)
     t.pendown()
+
+    t.forward(startSize)
     drawTree(t, startSize, startDepth)
 
     t.getscreen()._root.mainloop()
@@ -27,7 +32,7 @@ def drawTree(t, size, depth):
     
     ## Tree
     ##t.begin_fill()
-    t.forward(size)
+    #t.forward(size)
     t.right(angle)
     t.forward(size)
     ## Right
@@ -39,7 +44,7 @@ def drawTree(t, size, depth):
     drawTree(t, size*divF, depth-1)
     t.backward(size)
     t.right(angle)
-    t.backward(size)
+    #t.backward(size)
     ##t.end_fill()
 
     ## Right branch
